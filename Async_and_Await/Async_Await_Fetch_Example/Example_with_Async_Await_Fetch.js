82a14e235ff598e20400bd130dc24a809529f513
Sauraj-mrinal <msauraji@gmail.com>
2023-12-16 22:49:38 +0530

    Add

--- Async_and_Await/Async_Await_Fetch_Example/Example_with_Async_Await_Fetch.js
+++ Async_and_Await/Async_Await_Fetch_Example/Example_with_Async_Await_Fetch.js
@@ -4,18 +4,18 @@
 //----------------------------------------------------------------
 let result = document.getElementById('result1');
 
-// let Laptop = {
-//     Brand: 'MSI',
-//      SSD: '1TB',
-//      Color: 'Black',
-//      Processor: 'i5,9th Gen'
-// }
+let Laptop = {
+    Brand: 'MSI',
+     SSD: '1TB',
+     Color: 'Black',
+     Processor: 'i5,9th Gen'
+}
 
-// let BuyLaptop = new Promise((resolve , reject )=>{
-//     setTimeout(()=>{
-//          resolve(Laptop)
-//     },3000)
-// })
+let BuyLaptop = new Promise((resolve , reject )=>{
+    setTimeout(()=>{
+         resolve(Laptop)
+    },3000)
+})
 //now we will create a function 
 function fetch1(){
     console.log('working');
@@ -30,3 +30,34 @@ function fetch1(){
     //  })
     
 }
+
+//----------------------------------------------------------------
+//                         Ex - 02
+//                        using Asyns await
+//----------------------------------------------------------------
+let result2 = document.getElementById("result2");
+async function fetch2(){
+
+    result2.innerText = "fetching Data.."
+    let data = await BuyLaptop;
+    result2.innerText = JSON.stringify(data);
+}
+
+
+//----------------------------------------------------------------
+//                         Ex - 03
+//                        using Fetch Api
+//----------------------------------------------------------------
+
+let result3 = document.getElementById("result3");
+let BuyLaptop2  = fetch("https://jsonplaceholder.typicode.com/posts")
+.                 then(response => response.json())
+
+
+    function fetch3(){
+        //result3.innerText = "Fetching Data";
+        //promise
+        BuyLaptop2.then(res => {
+         result3.innerText=JSON.stringify(res)
+        })
+    }
