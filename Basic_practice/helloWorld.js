be6612d81e6fc6d02567ef35eeab760d8d220ff0
msauraji <msauraji@gmail.com>
2024-09-28 08:22:35 +0530

    Add

---
+++ Basic_practice/helloWorld.js
@@ -0,0 +1,7 @@
+// print hello world using conslole.log
+// print same by using alter()
+// ptint the same msg using docment.write 
+
+console.log("hello world ");
+alter("hello world");
+document.write("hello world");
